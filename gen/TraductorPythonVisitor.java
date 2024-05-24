f16e2cb9e24d31493612fa2052f52830e971a939
julesmf94 <jmagnin@unal.edu.co>
2024-05-24 12:47:51 -0500

    para desde incr

--- gen/TraductorPythonVisitor.java
+++ gen/TraductorPythonVisitor.java
@@ -3,13 +3,13 @@ public class TraductorPythonVisitor extends LatinoBaseVisitor{
     public Object visitSi(LatinoParser.SiContext ctx) {
         System.out.print("if ");
         visitExp(ctx.exp());
-        System.out.println(":");
-
+        System.out.println(": \n   ");
+        visitSecnovac(ctx.secnovac());
         for(int i = 0; i < ctx.osi().toArray().length; i++) {
             visitOsi(ctx.osi(i));
         }
 
-        return visitChildren(ctx);
+        return 0;
     }
 
     @Override
@@ -40,5 +40,57 @@ public class TraductorPythonVisitor extends LatinoBaseVisitor{
     public Object visitVal(LatinoParser.ValContext ctx) {
         System.out.print(ctx.getText());
         return super.visitVal(ctx);
+    }
+
+    @Override
+    public Object visitIncr(LatinoParser.IncrContext ctx) {
+        if (ctx.INCR().getText().equals("++")) {
+            System.out.println(ctx.ID() + " += 1");
+        } else {
+            System.out.println(ctx.ID() + " -= 1");
+        }
+        return 0;
+    }
+
+    @Override
+    public Object visitDesde(LatinoParser.DesdeContext ctx) {
+//        if(ctx.asig(0).ID().toArray().length > 1) {
+//            print
+//        }
+        System.out.print("for ");
+        System.out.print(ctx.asig(0).ID(0).getText());
+        System.out.print(" in range(");
+        System.out.print(ctx.asig(0).exp(0).getText());
+        System.out.print(",");
+        if (ctx.exp().OP(0).getText().equals("<=")){
+            int nombre = Integer.parseInt(ctx.exp().exp(0).getText()) + 1;
+            System.out.print(nombre);
+        }
+        else {
+            System.out.print(ctx.exp().exp(0).getText());
+        }
+        System.out.print("):");
+        visitSec(ctx.sec());
+
+        return 0;
+    }
+
+    public Object visitPara(LatinoParser.ParaContext ctx) {
+//        if(ctx.asig(0).ID().toArray().length > 1) {
+//            print
+//        }
+        System.out.print("for ");
+        System.out.print(ctx.ID().getText());
+        System.out.print(" in range(");
+        System.out.print(ctx.paraargs().REAL(0).getText());
+        if(ctx.paraargs().REAL().toArray().length > 1){
+            for(int i = 1; i < ctx.paraargs().REAL().toArray().length; i++) {
+                System.out.print(",");
+                System.out.print(ctx.paraargs().REAL(i).getText());
+            }
+        }
+        System.out.print("): \n");
+        visitSec(ctx.sec());
+        return 0;
     }
 }
